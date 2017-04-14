676dd8de2770bce344ead7461cf9fb3a8fb9e503
bamaisfunny <theantman15@yahoo.com>
2017-04-13 21:34:05 -0700

    Initial Commit - For the Ported Salsa App
    Initial Commit - For the Ported Salsa App

---
+++ as-salsa-ported/Salsa/Salsa/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Salsa
+{
+    [Activity(Label = "Salsa", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += Button_Click;
+          
+        }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            StartActivity(typeof(Recipe));
+        }
+    }
+}
+
---
+++ as-salsa-ported/Salsa/Salsa/Recipe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Salsa
+{
+    [Activity(Label = "Recipe")]
+    public class Recipe : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.recipe);
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
